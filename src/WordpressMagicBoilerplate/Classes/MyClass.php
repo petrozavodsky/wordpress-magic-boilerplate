94e0567b7d201e1e155d97406f8e646cbaa9e151
petrozavodsky <petrozavodsky@gmail.com>
2017-04-04 21:03:38 +0300

    Change assets added

--- src/WordpressMagicBoilerplate/Classes/MyClass.php
+++ src/WordpressMagicBoilerplate/Classes/MyClass.php
@@ -6,10 +6,9 @@ namespace WordpressMagicBoilerplate\Classes {
 
 	class MyClass {
 		use Assets;
+		private $state;
 		private $post_type = 'post';
-		public $state;
 		private $base_name;
-		private $file;
 
 		/**
 		 * Card constructor.
@@ -19,16 +18,14 @@ namespace WordpressMagicBoilerplate\Classes {
 		function __construct( $state ) {
 			$this->state     = $state;
 			$this->base_name = $state->base_name;
-			$this->file      = $state->file;
-
 			add_action( "get_header", array( $this, "router" ) );
-			$this->addCss( 'MyClassStyle', 'wp_footer' );
+			$this->addCss( 'MyClassStyle', 'footer' );
 		}
 
 		public function router() {
 			if ( is_singular( $this->post_type ) ) {
-				$this->state->addCss( "Single-style", "footer" );
-				$this->state->addJs( "Single-script", "footer", array( "jquery" ), "1.0.0" );
+				$this->addCss( "Single-style", "footer" );
+				$this->addJs( "Single-script", "footer", array( "jquery" ), "1.0.0" );
 			}
 		}
 	}

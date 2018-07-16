044392220dee85c50579b085ce833e7a0003f4ac
petrozavodsky <petrozavodsky@gmail.com>
2018-07-16 10:34:42 +0300

    Composer ready

--- public/js/WordpressMagicBoilerplate-AjaxOut2.js
+++ public/js/WordpressMagicBoilerplate-AjaxOut2.js
@@ -1,6 +1,8 @@
 var $ = jQuery.noConflict();
 
 $(document).ready(function () {
+    console.log(WordpressMagicBoilerplate_AjaxOut2__vars.ajax_url_action);
+
     $.get(WordpressMagicBoilerplate_AjaxOut2__vars.ajax_url_action, function (result) {
         console.log(result);
     });

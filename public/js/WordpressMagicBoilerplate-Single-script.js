cb3790253e71455053816180bfb9dcee35a9a13a
petrozavodsky <petrozavodsky@gmail.com>
2019-08-28 11:51:56 +0300

    Fix

--- public/js/WordpressMagicBoilerplate-Single-script.js
+++ public/js/WordpressMagicBoilerplate-Single-script.js
@@ -1,12 +1,12 @@
 var $ = jQuery.noConflict();
 
-var sd_cart_ajax_action = {
+var WordpressMagicBoilerplateSingleScript = {
     run: function () {
     }
 
 };
 
 $(document).ready(function () {
-    sd_cart_ajax_action.run();
+    WordpressMagicBoilerplateSingleScript.run();
 });
 

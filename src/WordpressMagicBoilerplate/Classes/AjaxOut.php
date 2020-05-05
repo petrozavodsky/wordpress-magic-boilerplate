055dd9c8d955eb8a8ef9a4ac33185e30f03cd400
petrozavodsky <petrozavodsky@gmail.com>
2020-05-05 23:21:40 +0300

    New ajax example 1-3

--- src/WordpressMagicBoilerplate/Classes/AjaxOut.php
+++ src/WordpressMagicBoilerplate/Classes/AjaxOut.php
@@ -5,33 +5,36 @@ namespace WordpressMagicBoilerplate\Classes;
 use WordpressMagicBoilerplate\Utils\Ajax;
 use WordpressMagicBoilerplate\Utils\Assets;
 
-class AjaxOut extends Ajax {
+class AjaxOut extends Ajax
+{
 
-	use Assets;
+    use Assets;
 
-	public function init($actionName ) {
-		$this->addJsCss( $actionName );
+    public function init($actionName)
+    {
+        $this->addJsCss($actionName);
+    }
 
-	}
+    public function callback($request)
+    {
+        $json = ["out" => "AJAX Content 1"];
+        wp_send_json($json);
+    }
 
-	public function callback( $request ) {
-		$json = [ "out" => "AJAX Content" ];
-		wp_send_json( $json );
-	}
+    private function addJsCss($action)
+    {
+        $handle = $this->addJs(
+            $action,
+            'header',
+            ['jquery']
+        );
 
-	private function addJsCss( $action ) {
-		$handle = $this->addJs(
-			$action,
-			'header',
-			[ 'jquery' ]
-		);
-
-		$this->varsAjax(
-			$handle,
-			[
-				'ajaxUrl'        => $this->ajaxUrl,
-				'ajaxUrlAction' => $this->ajaxUrlAction,
-			]
-		);
-	}
+        $this->varsAjax(
+            $handle,
+            [
+                'ajaxUrl' => $this->ajaxUrl,
+                'ajaxUrlAction' => $this->ajaxUrlAction,
+            ]
+        );
+    }
 }

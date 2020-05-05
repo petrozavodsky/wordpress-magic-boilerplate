daacc89c0578d8cb91bf67204fe5966e0c1553ab
petrozavodsky <petrozavodsky@gmail.com>
2020-05-05 22:59:02 +0300

    Script additional variables

--- core/WordpressMagicBoilerplate/Utils/Ajax.php
+++ core/WordpressMagicBoilerplate/Utils/Ajax.php
@@ -2,11 +2,18 @@
 
 namespace WordpressMagicBoilerplate\Utils;
 
-abstract class Ajax {
+abstract class Ajax
+{
+    use Assets;
 
     protected $ajaxUrl;
+
     protected $ajaxUrlAction;
+
     protected $action;
+
+    protected $js = false;
+
 
     /**
      * Ajax constructor.
@@ -15,24 +22,36 @@ abstract class Ajax {
      * @param string $type
      *
      */
-    public function __construct($actionName, $type = 'front' ) {
+    public function __construct($actionName, $type = 'front')
+    {
 
-        $this->action        = $actionName;
-        $this->ajaxUrl        = $this->createAjaxUrl();
-        $this->ajaxUrlAction = $this->createAjaxUrlAction( $actionName );
+        $this->action = $actionName;
+        $this->ajaxUrl = $this->createAjaxUrl();
+        $this->ajaxUrlAction = $this->createAjaxUrlAction($actionName);
 
-        $this->$type( $actionName );
+        $this->$type($actionName);
 
-        if ( method_exists( $this, 'init' ) ) {
-            $this->init( $actionName );
+        if (method_exists($this, 'init')) {
+            $this->init($actionName);
+        }
+
+        $this->assets();
+    }
+
+    protected function assets()
+    {
+        if ($this->js) {
+            $jsHandle = $this->addJs($this->action);
+            $this->varsAjax($jsHandle);
         }
     }
 
     /**
      * @return string
      */
-    protected function createAjaxUrl() {
-        return admin_url( 'admin-ajax.php' );
+    protected function createAjaxUrl()
+    {
+        return admin_url('admin-ajax.php');
     }
 
     /**
@@ -40,8 +59,9 @@ abstract class Ajax {
      *
      * @return string
      */
-    protected function createAjaxUrlAction( $action ) {
-        return add_query_arg( [ 'action' => $action ], $this->ajaxUrl );
+    protected function createAjaxUrlAction($action)
+    {
+        return $this->ajaxUrl;
     }
 
     /**
@@ -50,42 +70,43 @@ abstract class Ajax {
      * @param array $data
      *
      */
-    public function varsAjax( $handle, $data ) {
+    public function varsAjax($handle, $data = [])
+    {
+        $data = wp_parse_args(
+            $data,
+            [
+                'action_url' => add_query_arg(['action' => $this->action], $this->ajaxUrl)
+            ]
+        );
 
-        $actions = [
-            'login_enqueue_scripts',
-            'wp_enqueue_scripts',
-            'admin_enqueue_scripts'
-        ];
-
-        foreach ($actions as $action) {
-
-            add_action($action, function () use ($data, $handle) {
-                wp_localize_script(
-                    $handle,
-                    str_replace('-', '_', $handle . "__vars"),
-                    $data
-                );
-            }, 80);
-        }  
+        add_action('wp_enqueue_scripts', function () use ($data, $handle) {
+            wp_localize_script(
+                $handle,
+                str_replace('-', '_', $handle . "__vars"),
+                $data
+            );
+        }, 80);
     }
 
-    public function front($actionName, $callback = 'payload' ) {
-        add_action( 'wp_ajax_' . $actionName, [ $this, $callback]  );
-        add_action( 'wp_ajax_nopriv_' . $actionName, [ $this, $callback ] );
+    public function front($actionName, $callback = 'payload')
+    {
+        add_action('wp_ajax_' . $actionName, [$this, $callback]);
+        add_action('wp_ajax_nopriv_' . $actionName, [$this, $callback]);
     }
 
 
-    public function admin($actionName, $callback = 'payload' ) {
-        add_action( 'wp_ajax_' . $actionName, [$this, $callback] );
+    public function admin($actionName, $callback = 'payload')
+    {
+        add_action('wp_ajax_' . $actionName, [$this, $callback]);
     }
 
-    public function payload() {
+    public function payload()
+    {
         $request = $_REQUEST;
-        unset( $request['action'] );
-        $this->callback( $request );
+        unset($request['action']);
+        $this->callback($request);
     }
 
-    abstract public function callback( $request );
+    abstract public function callback($request);
 
 }

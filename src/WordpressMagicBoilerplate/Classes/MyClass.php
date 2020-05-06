179f492aef5dc6aefdfe07f16798cd83574c8e89
petrozavodsky <petrozavodsky@gmail.com>
2020-05-06 23:06:49 +0300

    fix quotation style

--- src/WordpressMagicBoilerplate/Classes/MyClass.php
+++ src/WordpressMagicBoilerplate/Classes/MyClass.php
@@ -4,58 +4,61 @@ namespace WordpressMagicBoilerplate\Classes;
 
 use WordpressMagicBoilerplate\Utils\Assets;
 
-class MyClass {
+class MyClass
+{
 
-	use Assets;
-	private $state;
-	private $typePost = 'post';
-	private $baseName;
+    use Assets;
 
-	/**
-	 *
-	 * @param object $state
-	 */
-	public function __construct( $state ) {
-		$this->state     = $state;
-		$this->baseName = $state->baseName;
-		add_action( "get_header", [ $this, "router" ] );
+    private $state;
+    private $typePost = 'post';
+    private $baseName;
 
-	}
+    /**
+     *
+     * @param object $state
+     */
+    public function __construct($state)
+    {
+        $this->state = $state;
+        $this->baseName = $state->baseName;
+        add_action('get_header', [$this, 'router']);
 
-	public function router() {
+    }
 
-		//Add css in footer
-		$this->addCss(
-			'MyClassStyle',
-			'footer'
-		);
+    public function router()
+    {
 
-		//Add custom url css
-		$this->addCss(
-			"Common-style",
-			"header",
-			[],
-			$this->state->version,
-			$this->state->pluginUrl . $this->state->cssPatch . "Common-style.css"
-		);
+        //Add css in footer
+        $this->addCss(
+            'MyClassStyle'
+        );
 
-		//page type rout
-		if ( is_singular( $this->typePost ) ) {
+        //Add custom url css
+        $this->addCss(
+            'Common-style',
+            'footer',
+            [],
+            $this->state->version,
+            $this->state->pluginUrl . $this->state->cssPatch . 'Common-style.css'
+        );
 
-			//Add auto url css
-			$this->addCss(
-				"Single-style",
-				"footer"
-			);
+        //page type rout
+        if (is_singular($this->typePost)) {
 
-			//Add auto url js
-			$this->addJs(
-				"Single-script",
-				"footer",
-				[ "jquery" ],
-				"1.0.3"
-			);
-		}
-	}
+            //Add auto url css
+            $this->addCss(
+                'Single-style',
+                'footer'
+            );
+
+            //Add auto url js
+            $this->addJs(
+                'Single-script',
+                'footer',
+                ['jquery'],
+                '1.0.3'
+            );
+        }
+    }
 
 }
